12badeffb52e7014b7a271a503c33e28dc32d5bf
Reem Kamal Ghoniem <reemkamal111@gmail.com>
2023-11-06 04:41:09 +0200

    task 4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,50 @@
+#!/usr/bin/node
+const res = {};
+$(document).ready(function () {
+  $('input').prop('checked', false);
+  $('input').on('change', function () {
+    let text = '';
+    let key = $(this).data('id');
+    const value = $(this).data('name');
+    if ($(this).is(':checked')) {
+      res[key] = value;
+    } else {
+      delete res[key];
+    }
+    for (key in res) {
+      if (text === '') {
+        text = res[key];
+      } else {
+        text = text + ', ' + res[key];
+      }
+    }
+    console.log(text);
+    $('.amenities h4').text(text);
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    console.log(data);
+    if (data.status === 'OK') {
+      console.log('data');
+      $('div#api_status').addClass('available');
+    } else {
+      console.log('red');
+      $('div#api_status').removeClass('available');
+    }
+  });
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        let place = data[i];
+        $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+      }
+    }
+  });
+}
+
+);
